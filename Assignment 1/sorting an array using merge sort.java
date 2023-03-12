37f18c0440fec431953a04f1043198b1fa92deab
krbittu2024 <bittuk85btechcse2024@kccitm.edu.in>
2023-03-12 19:22:17 +0530

    sorting

---
+++ Assignment 1/sorting an array using merge sort.java
@@ -0,0 +1,48 @@
+public class MergeSort {
+    
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+        mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void mergeSort(int[] arr) {
+        int n = arr.length;
+        if (n <= 1) {
+            return;
+        }
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            right[i - mid] = arr[i];
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+    
+    public static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+}
