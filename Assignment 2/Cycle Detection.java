5b8a0d05b42e4053c72d367ad042e243c9f820ac
krbittu2024 <106546621+krbittu2024@users.noreply.github.com>
2023-03-12 19:09:01 +0530

    Update Cycle Detection.java

--- Assignment 2/Cycle Detection.java
+++ Assignment 2/Cycle Detection.java
@@ -64,7 +64,20 @@ public class Solution {
      *
      */
     static boolean hasCycle(SinglyLinkedListNode head) {
-
+        if(head == null)return false;
+        
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = slow.next;
+        while(fast != null && fast.next != null){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+            
+             if(slow.next == fast){
+                return true;
+            }
+        }
+        return false;
 
     }
 
